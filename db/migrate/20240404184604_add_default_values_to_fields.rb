d9064e5dcc1ceff2ed305da342a992fb6121e2e6
Sanjar <softswiss@Softswiss-MacBook-Pro.local>
2024-04-05 20:43:03 +0200

    regenerating_migrations

--- db/migrate/20240404184604_add_default_values_to_fields.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultValuesToFields < ActiveRecord::Migration[6.1]
-  def change
-    change_column_default(:answers, :status, from: true, to: false)
-    change_column_default(:tests, :level, from: 0, to: 1)
-  end
-end
--- db/migrate/20240405123327_create_questions.rb
+++ db/migrate/20240405123327_create_questions.rb
@@ -1,9 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[6.1]
   def change
     create_table :questions do |t|
-      t.text :question_text
-      t.string :answer_options
-      t.string :correct_answer
+      t.text :body, null: false
+      t.references :test, null: false, foreign_key: true
 
       t.timestamps
     end
