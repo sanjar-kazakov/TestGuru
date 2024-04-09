f0ca27fa6f93d7db7aaedf37b53445a964191a16
Sanjar <softswiss@Softswiss-MacBook-Pro.local>
2024-04-09 08:38:36 +0200

    remarks correction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def history_by_level(level)
-    Test.joins('JOIN user_answers ON tests.id = user_answers.user_id')
+    Test.joins('JOIN user_answers ON tests.id = user_answers.test_id')
       .where('user_answers.user_id = :id AND tests.level = :level', id: self.id, level: level).pluck(:title)
   end
 end
