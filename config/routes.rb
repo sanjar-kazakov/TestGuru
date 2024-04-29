eb17b82e17057ff43e543a6bd994888ee1372411
sanjar-kazakov <sanjarrr28@gmail.com>
2024-04-29 17:53:31 +0200

    new

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'tests#index'
 
   resources :tests do
-    resources :questions
+    resources :questions, :except => [:index, :delete]
   end
 
   delete '/tests/:test_id/questions/:question_id', to: 'questions#destroy', as: 'delete_test_question'
