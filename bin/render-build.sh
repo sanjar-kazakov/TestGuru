f0ec63282a36f0b6dd73dc3fa1dd57e4772b2aea
sanjar-kazakov <sanjarrr28@gmail.com>
2024-07-06 16:04:13 +0200

    added seed.rb to the render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
-# exit on error
-set -o errxit
+# Exit on error
+set -o errexit
 
+# Install dependencies
 bundle install
 yarn install
+
+# Precompile assets
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+# Run database migrations
 bundle exec rake db:migrate
+
+# Seed the database with data from seeds.rb
+bundle exec rake db:seed
