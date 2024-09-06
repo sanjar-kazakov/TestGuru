ce2ca429574a24c0fc2a66480af62b20c8a979be
sanjar-kazakov <sanjarrr28@gmail.com>
2024-09-06 19:27:46 +0200

    user can set empty answer

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -11,19 +11,14 @@ class UserAnswersController < ApplicationController
   end
 
   def update
-    if params[:answer_ids].present? || @user_answer.expired?
-      @user_answer.accept!(params[:answer_ids] || [])
+    @user_answer.accept!(params[:answer_ids]) unless @user_answer.expired?
 
-      if @user_answer.completed? || @user_answer.expired?
-        @user_answer.update_success_status
-        new_badges = BadgeService.new(@user_answer).call if @user_answer.success?
-        TestsMailer.completed_test(@user_answer).deliver_now
-        redirect_to result_user_answer_path(@user_answer, badges: new_badges)
-      else
-        render :show
-      end
+    if @user_answer.completed? || @user_answer.expired?
+      @user_answer.update_success_status
+      new_badges = BadgeService.new(@user_answer).call if @user_answer.success?
+      TestsMailer.completed_test(@user_answer).deliver_now
+      redirect_to result_user_answer_path(@user_answer, badges: new_badges)
     else
-      flash.now[:alert] = t('.failure')
       render :show
     end
   end
